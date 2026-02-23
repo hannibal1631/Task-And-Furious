f86720dcbe276deeffa100527ba78c9cd70ded9c
hannibal1631 <mukherjeerupak860@gmail.com>
2026-02-23 13:29:47 +0530

    test commits

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function App() {
-  return <div className='text-2xl text-red-500'>what a wonderful day</div>;
+  return (
+    <>
+      <div className='text-2xl text-red-500'>what a wonderful day</div>;
+      <div className='text-2xl text-red-500'>apes together strong</div>;
+    </>
+  );
 }
 
 export default App;
