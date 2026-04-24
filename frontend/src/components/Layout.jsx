a82b9e9391a7723615d56cc126fcf64f148a911f
hannibal1631 <mukherjeerupak860@gmail.com>
2026-04-24 12:06:57 +0530

    add minor UI changes

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -219,9 +219,9 @@ function Layout() {
         {/* header ends here */}
 
         {/* BODY */}
-        <div className='flex flex-1 items-stretch gap-6 bg-red-500 py-4 px-7 overflow-hidden'>
+        <div className='flex flex-1 items-stretch gap-4 bg-red-500 py-4 px-7 overflow-hidden'>
           {/* SIDEBAR */}
-          <div className='flex flex-col w-[5%] justify-between items-center bg-blue-600 px-8 py-6 rounded-xl'>
+          <div className='flex flex-col w-[3%] justify-between items-center bg-blue-600 px-8 py-6 rounded-xl'>
             <div className='flex flex-col gap-6'>
               <NavLink to='/dashboard' title='dashboard'>
                 <FontAwesomeIcon
@@ -275,7 +275,7 @@ function Layout() {
           </div>
 
           {/* MAIN CONTENT */}
-          <div className='relative flex flex-col w-[95%] md:px-8 md:py-6 px-1 py-1 bg-blue-600 rounded-xl overflow-y-auto no-scrollbar'>
+          <div className='relative flex flex-col w-[97%] md:px-8 md:py-6 px-1 py-1 bg-blue-600 rounded-xl overflow-y-auto no-scrollbar'>
             {/* <Outlet context={{ setView }} /> */}
             <div className='flex-1 overflow-y-auto no-scrollbar px-8 py-6 sm:p-0 pb-24'>
               <Outlet
