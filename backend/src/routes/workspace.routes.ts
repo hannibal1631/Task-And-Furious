1d6fb324a17be06f5878885c55ccb26ef7679180
Riktam Nandi <riktamrdxnandi@gmail.com>
2026-05-02 20:19:45 +0530

    add getWorkspaceByUserId endpoint to fetch workspaces by user ID

--- backend/src/routes/workspace.routes.ts
+++ backend/src/routes/workspace.routes.ts
@@ -2,11 +2,14 @@ import { Router } from "express";
 import {
   addTeammates,
   createWorkspace,
+  getWorkspaceByUserId,
 } from "../controllers/workspace.controllers";
 
 const router = Router();
 
 router.route("/:userId").post(createWorkspace);
 router.route("/:workspaceId/members/:userId").post(addTeammates);
+
+router.route("/user/:userId").get(getWorkspaceByUserId);
 
 export default router;
