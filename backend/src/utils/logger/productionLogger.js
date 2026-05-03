9d959c3a4884092d817432b27ef8fda72695bac4
Riktam Nandi <riktamrdxnandi@gmail.com>
2026-05-03 20:21:18 +0530

    implement winston logging for development and production environments

--- backend/src/utils/logger/productionLogger.js
+++ backend/src/utils/logger/productionLogger.js
@@ -1,3 +1,23 @@
-const productionLogger = () => {};
+import * as winston from "winston";
+
+const { format, transports, createLogger } = winston;
+const { combine, timestamp, printf, label, simple } = format;
+
+const myFormat = printf(({ level, message, timestamp }) => {
+  return `[${level}] ${timestamp} ${message}`;
+});
+
+const productionLogger = () => {
+  return createLogger({
+    level: "debug",
+    format: combine(timestamp(), errors({ stack: true }), myFormat),
+    // defaultMeta: { service: "user-service" },
+    transports: [
+      new transports.Console(),
+      new transports.File({ filename: "logs/errors.log", level: "error" }),
+      new transports.File({ filename: "logs/combined.log", level: "error" }),
+    ],
+  });
+};
 
 export default productionLogger;
